ef824267a18a249a08c812f88fdf3e895a2f8a50
Elvis.Shi <Singosgu@gmail.com>
2022-12-30 11:51:38 +0800

    V 0.1.5

---
+++ command/management/commands/createapp.py
@@ -0,0 +1,88 @@
+from django.conf import settings
+import os
+
+
+def create_config_ini(path):
+    config_ini = os.path.join(path, "config.ini")
+    with open(config_ini, "w") as f:
+        f.write("\n")
+    f.close()
+
+
+def create_views_py(path):
+    views_py = os.path.join(path, "views.py")
+    with open(views_py, "w") as f:
+        f.write("\n")
+    f.close()
+
+
+def create_urls_py(path):
+    urls_py = os.path.join(path, "urls.py")
+    with open(urls_py, "w") as f:
+        f.write("from django.urls import path, re_path, include\n")
+        f.write("\n")
+        f.write("urlpatterns = [\n")
+        f.write("]\n")
+    f.close()
+
+
+def create_test_py(path):
+    test_py = os.path.join(path, "test.py")
+    with open(test_py, "w") as f:
+        f.write("from django.test import TestCase\n")
+        f.write("\n")
+        f.write("# Create your tests here.\n")
+    f.close()
+
+
+def create_models_py(path):
+    models_py = os.path.join(path, "models.py")
+    with open(models_py, "w") as f:
+        f.write("from django.db import models\n")
+        f.write("\n")
+        f.write("# Create your models here.\n")
+    f.close()
+
+
+def create_apps_py(path, name):
+    apps_py = os.path.join(path, "apps.py")
+    with open(apps_py, "w") as f:
+        f.write("from django.apps import AppConfig\n")
+        f.write("\n")
+        f.write(f"class {name.capitalize()}Config(AppConfig):\n")
+        f.write(f"    name = '{name}'\n")
+    f.close()
+
+
+def create_admin_py(path):
+    admin_py = os.path.join(path, "admin.py")
+    with open(admin_py, "w") as f:
+        f.write("from django.contrib import admin\n")
+        f.write("\n")
+        f.write("# Register your models here.\n")
+    f.close()
+
+
+def get_app_name(name):
+    for app in name:
+        app_dir = os.path.join(settings.BASE_DIR, app)
+        if os.path.exists(app_dir):
+            return 'APP has been exists'
+        else:
+            os.mkdir(app_dir)
+            database_dir = os.path.join(app_dir, 'migrations')
+            os.mkdir(database_dir)
+            database_init_py = os.path.join(database_dir, "__init__.py")
+            with open(database_init_py, "w") as f:
+                f.write("\n")
+            init_py = os.path.join(app_dir, "__init__.py")
+            with open(init_py, "w") as f:
+                f.write("\n")
+            create_admin_py(app_dir)
+            create_apps_py(app_dir, app)
+            create_models_py(app_dir)
+            create_urls_py(app_dir)
+            create_urls_py(app_dir)
+            create_views_py(app_dir)
+            create_config_ini(app_dir)
+            return 'Success create app'
---
+++ command/management/commands/gettoken.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+import os
+import re
+
+
+def validate_mail(email):
+    if len(email) >= 7:
+        email_check = re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", email)
+        if email_check is not None:
+            return True
+        return False
+
+
+def get_token(name):
+    token_ini = os.path.join(settings.BASE_DIR, 'token.ini')
+    with open(token_ini, "w") as f:
+        f.write("[token]\n")
+        f.write(f"email = {name}\n")
+        f.write(f"token = {name}\n")
+    f.close()
+    return "Success get developer's token"
