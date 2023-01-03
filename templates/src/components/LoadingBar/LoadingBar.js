6940b3ebf6a622f8b709748fa7351822209b6cb0
Elvis.Shi <Singosgu@gmail.com>
2023-01-04 07:50:13 +0800

    V 0.1.5

---
+++ templates/src/components/LoadingBar/LoadingBar.js
@@ -0,0 +1,9 @@
+import { LoadingBar } from 'quasar'
+
+LoadingBar.setDefaults({
+  color: 'Bomiot-LoadingBar-color',
+  size: '2.3px',
+  position: 'top'
+})
+
+export default LoadingBar
--- templates/src/router/index.js
+++ templates/src/router/index.js
@@ -1,15 +1,7 @@
 import { route } from 'quasar/wrappers'
 import { createRouter, createMemoryHistory, createWebHistory, createWebHashHistory } from 'vue-router'
 import routes from './routes'
-
-/*
- * If not building with SSR mode, you can
- * directly export the Router instantiation;
- *
- * The function below can be async too; either use
- * async/await or return a Promise which resolves
- * with the Router instance.
- */
+import LoadingBar from 'components/LoadingBar/LoadingBar'
 
 export default route(function (/* { store, ssrContext } */) {
   const createHistory = process.env.SERVER
@@ -19,11 +11,15 @@ export default route(function (/* { store, ssrContext } */) {
   const Router = createRouter({
     scrollBehavior: () => ({ left: 0, top: 0 }),
     routes,
-
-    // Leave this as is and make changes in quasar.conf.js instead!
-    // quasar.conf.js -> build -> vueRouterMode
-    // quasar.conf.js -> build -> publicPath
     history: createHistory(process.env.VUE_ROUTER_BASE)
+
+  })
+
+  Router.beforeEach((to, from, next) => {
+    LoadingBar.start()
+  })
+  Router.afterEach((to, from, failure) => {
+    LoadingBar.stop()
   })
   return Router
 })
